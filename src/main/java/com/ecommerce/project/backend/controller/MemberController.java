802d5f07bf8497363275034fdc96c541524c53e7
JS <wnlgod78@gmail.com>
2025-11-11 13:00:36 +0900

    상세 주소 입력 로직 추가 (DB)

--- src/main/java/com/ecommerce/project/backend/controller/MemberController.java
+++ src/main/java/com/ecommerce/project/backend/controller/MemberController.java
@@ -21,7 +21,7 @@ public class MemberController {
             memberService.signup(request);
             return ResponseEntity.ok("회원가입 성공!");
         } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(409).body(" " + e.getMessage());
+            return ResponseEntity.status(409).body("회원가입 실패" + e.getMessage());
         }
     }
 }
--- src/main/java/com/ecommerce/project/backend/service/MemberService.java
+++ src/main/java/com/ecommerce/project/backend/service/MemberService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class MemberService {
+public class  MemberService {
 
     private final MemberRepository memberRepository;
     private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
